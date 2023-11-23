bcfec20e145673135bc5341129ae0ed499231f09
Markus <markus@bananenfisch.net>
2023-11-23 22:19:36 +0100

    datatypes

--- abend2/basic1.php
+++ abend2/basic1.php
@@ -7,8 +7,22 @@
 </head>
 <body>
     <?php
+    /*
+        $book_count = 4.13;
+        var_dump($book_count);
+        $book_count = 3;
+        var_dump($book_count);
+    */
+        $book_count = 5;
+
+        echo 'Wir haben ' . $book_count . ' Bücher im Sortiment';
+
+        echo 'Wir haben ';
+        echo $book_count;
+        echo ' Bücher im Sortiment';
+
         // Das ist ein einzeiliger Kommentar
-        echo 'Zeichenkette'
+        echo 'Zeichenkette';
 
         /*
             Das ist ein mehrzeiliger Kommentar...
---
+++ abend2/datatypes.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // skalare datentypen
+
+        // int
+        var_dump(123);
+        // float
+        var_dump(5.9);
+        // string
+        var_dump('abc');
+        // boolean
+        var_dump(false);
+
+        // null
+        var_dump(null);
+    ?>
+</body>
+</html>
