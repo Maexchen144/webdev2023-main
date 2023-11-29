333571bd6f941b7ad497be8fc850abd1f73f3714
Markus <markus@bananenfisch.net>
2023-11-29 20:37:49 +0100

    form-submit

---
+++ abend4/assoc.php
@@ -0,0 +1,14 @@
+<?php
+$array_indiziert = [ 'Markus', 'Huber', 'Wien', 1000, 'Straße' ];
+// echo $array_indiziert[0];
+
+$array_assoc = [
+    'firstname' => 'Markus',
+    'lastname' => 'Huber',
+    'city' => 'Wien'
+];
+// echo $array_assoc['firstname'];
+
+var_dump($array_indiziert);
+
+var_dump($array_assoc);
---
+++ abend4/formular/save.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Vielen Dank, Ihre Eingaben sind:<br>
+    Ihr Vorname: <?php echo $_POST['firstname']; ?><br>
+    Ihr Nachname: <?php echo $_POST['lastname']; ?><br>
+</body>
+</html>
